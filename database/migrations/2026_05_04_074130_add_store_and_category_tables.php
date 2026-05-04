3e272c4b1bee48eb59854b0dd5300e93cbcc5a60
josep <josep98cli@gmail.com>
2026-05-04 10:54:15 +0200

    feat: add 'active' column to store and category tables for status management

--- database/migrations/2026_05_04_074130_add_store_and_category_tables.php
+++ database/migrations/2026_05_04_074130_add_store_and_category_tables.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->string('slug')->unique();
             $table->json('match_keywords');
+
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
